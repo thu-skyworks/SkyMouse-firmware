bcedf2abf6519954d078a0c9680ed63ea73c2908
zhang <zz593141477@gmail.com>
2015-07-10 16:28:58 +0800

    remove commented code

--- Project/usb-msd/src/mass_mal.c
+++ Project/usb-msd/src/mass_mal.c
@@ -61,11 +61,6 @@ uint16_t MAL_Init(uint8_t lun)
       Status = SD_Init();
       DBG_MSG("SD_Init()=%d", Status);
       break;
-// #ifdef USE_STM3210E_EVAL
-//     case 1:
-//       NAND_Init();
-//       break;
-// #endif
     default:
       return MAL_FAIL;
   }
@@ -85,20 +80,13 @@ uint16_t MAL_Write(uint8_t lun, uint32_t Memory_Offset, uint32_t *Writebuff, uin
   {
     case 0:
     Status = SD_WriteMultiBlocks((uint8_t*)Writebuff, Memory_Offset, Transfer_Length,1);
-// #if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
     Status = SD_WaitWriteOperation();  
     while(SD_GetStatus() != SD_TRANSFER_OK);
       if ( Status != SD_OK )
       {
         return MAL_FAIL;
       }      
-// #endif /* USE_STM3210E_EVAL ||USE_STM32L152D_EVAL*/      
       break;
-// #ifdef USE_STM3210E_EVAL
-//     case 1:
-//       NAND_Write(Memory_Offset, Writebuff, Transfer_Length);
-//       break;
-// #endif /* USE_STM3210E_EVAL */  
     default:
       return MAL_FAIL;
   }
@@ -119,7 +107,6 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
   {
     case 0:
       SD_ReadMultiBlocks((uint8_t*)Readbuff, Memory_Offset, Transfer_Length, 1);
-// #if defined(USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
       Status = SD_WaitReadOperation();
       while(SD_GetStatus() != SD_TRANSFER_OK)
       {
@@ -129,14 +116,7 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
       {
         return MAL_FAIL;
       }
-// #endif /* USE_STM3210E_EVAL */      
       break;
-// #ifdef USE_STM3210E_EVAL
-//     case 1:
-//       NAND_Read(Memory_Offset, Readbuff, Transfer_Length);
-//       ;
-//       break;
-// #endif
     default:
       return MAL_FAIL;
   }
@@ -152,22 +132,10 @@ uint16_t MAL_Read(uint8_t lun, uint32_t Memory_Offset, uint32_t *Readbuff, uint1
 *******************************************************************************/
 uint16_t MAL_GetStatus (uint8_t lun)
 {
-// #ifdef USE_STM3210E_EVAL
-//   NAND_IDTypeDef NAND_ID;
   uint32_t DeviceSizeMul = 0, NumberOfBlocks = 0;
-// #else
-//   SD_CSD SD_csd;
-//   uint32_t DeviceSizeMul = 0;
-// #endif /* USE_STM3210E_EVAL */
-
-// #ifdef USE_STM32L152D_EVAL
-
-//   uint32_t NumberOfBlocks = 0;
-// #endif
 
   if (lun == 0)
   {
-// #if defined (USE_STM3210E_EVAL)  || defined(USE_STM32L152D_EVAL)
     if (SD_Init() == SD_OK)
     {
       SD_GetCardInfo(&mSDCardInfo);
@@ -192,40 +160,11 @@ uint16_t MAL_GetStatus (uint8_t lun)
       {
         return MAL_FAIL;
       }
-     
-// #else
-
-//     uint32_t temp_block_mul = 0;
-//     SD_GetCSDRegister(&SD_csd);
-//     DeviceSizeMul = SD_csd.DeviceSizeMul + 2;
-//     temp_block_mul = (1 << SD_csd.RdBlockLen)/ 512;
-//     Mass_Block_Count[0] = ((SD_csd.DeviceSize + 1) * (1 << (DeviceSizeMul))) * temp_block_mul;
-//     Mass_Block_Size[0] = 512;
-//     Mass_Memory_Size[0] = (Mass_Block_Count[0] * Mass_Block_Size[0]);
-// #endif /* USE_STM3210E_EVAL */
       Mass_Memory_Size[0] = Mass_Block_Count[0] * Mass_Block_Size[0];
-      // STM_EVAL_LEDOn(LED2);
       return MAL_OK;
 
-// #if defined (USE_STM3210E_EVAL) || defined(USE_STM32L152D_EVAL)
     }
-// #endif /* USE_STM3210E_EVAL */
   }
-// #ifdef USE_STM3210E_EVAL
-//   else
-//   {
-//     FSMC_NAND_ReadID(&NAND_ID);
-//     if (NAND_ID.Device_ID != 0 )
-//     {
-//       /* only one zone is used */
-//       Mass_Block_Count[1] = NAND_ZONE_SIZE * NAND_BLOCK_SIZE * NAND_MAX_ZONE ;
-//       Mass_Block_Size[1]  = NAND_PAGE_SIZE;
-//       Mass_Memory_Size[1] = (Mass_Block_Count[1] * Mass_Block_Size[1]);
-//       return MAL_OK;
-//     }
-//   }
-// #endif /* USE_STM3210E_EVAL */
-  // STM_EVAL_LEDOn(LED2);
   return MAL_FAIL;
 }
 
